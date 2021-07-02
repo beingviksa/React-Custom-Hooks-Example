8a6b6171fecc4a132e0b815cca86efa2722be22f
Vikram Singh <singhvikram143777@gmail.com>
2021-07-02 11:12:23 +0530

    React custom hook example

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Random from "./components/Random_V2";
+import Tag from "./components/Tag_V2";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Random GIF Application</h1>
+      <div className="main-container">
+        <Random />
+        <Tag />
+      </div>
+    </div>
+  );
+};
+
+export default App;
